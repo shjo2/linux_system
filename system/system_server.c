3d95e10b126d14642bc4fc14781d6069fec4b663
sungho <quine5286@gmail.com>
2024-11-20 00:02:35 +0900

    Initial commit

---
+++ system/system_server.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/prctl.h>
+
+#include <system_server.h>
+#include <gui.h>
+#include <input.h>
+#include <web_server.h>
+
+int system_server()
+{
+    printf("나 system_server 프로세스!\n");
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
+
+int create_system_server()
+{
+    pid_t systemPid;
+    const char *name = "system_server";
+
+    printf("여기서 시스템 프로세스를 생성합니다.\n");
+
+    /* fork 를 이용하세요 */
+    switch (systemPid = fork()) {
+    case -1:
+        printf("fork failed\n");
+    case 0:
+        if (prctl(PR_SET_NAME, (unsigned long) name) < 0)
+            perror("prctl()");
+        system_server();
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
---
+++ ui/input.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/prctl.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+#include <system_server.h>
+#include <gui.h>
+#include <input.h>
+#include <web_server.h>
+#include <execinfo.h>
+
+
+int input()
+{
+    printf("나 input 프로세스!\n");
+
+    /* 여기서 SIGSEGV 시그널 등록 */
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
+
+int create_input()
+{
+    pid_t systemPid;
+    const char *name = "input";
+
+    printf("여기서 input 프로세스를 생성합니다.\n");
+
+    /* fork 를 이용하세요 */
+    switch (systemPid = fork()) {
+    case -1:
+        printf("fork failed\n");
+    case 0:
+        if (prctl(PR_SET_NAME, (unsigned long) name) < 0)
+            perror("prctl()");
+        input();
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
